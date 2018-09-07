3c8bee3e509ac2bb8b95e3e50795b75198d40dea
Allen Roberts <allen.roberts1985@gmail.com>
2018-09-07 11:45:05 -0400

    Add project files.

---
+++ FindingPrimes/PrimeFinder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindingPrimes
+{
+    public class PrimeFinder
+    {
+        private static int[] _divisibles = new int[]
+        {
+            2,3,4,5,6,7,8,9,10
+        };
+
+        private static List<int> _primes = new List<int>();
+
+
+        public static bool IsPrime(int testNum)
+        {
+            bool prime = true;
+
+            //test if 1
+            if (testNum == 1 || testNum == 0)
+                return false;
+
+            //test if 2 through 10 are divisible and testNum doesn't equal 2 through 10
+            foreach(var i in _divisibles)
+                if (testNum % i == 0 && testNum != i)
+                    prime = false;
+
+            //test if testNum is divisible by another prime and testNum doesn't equal what is dividing it
+            foreach (var i in _primes)
+                if (testNum % i == 0 && testNum != i)
+                    prime = false;
+
+            //If it cannot disprove it's a prime it adds it to the prime list
+            if (!prime)
+                _primes.Add(testNum);
+
+            return prime;
+        }
+    }
+}
---
+++ FindingPrimes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FindingPrimes
+{
+    class Program
+    {
+        public const int LOWER_BOUNDS = 0;
+        public const int UPPER_BOUNDS = 2000;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prime Numbers Between {0} and {1}", LOWER_BOUNDS, UPPER_BOUNDS);
+            for (int i = LOWER_BOUNDS; i <= UPPER_BOUNDS; i++)
+                if (PrimeFinder.IsPrime(i))
+                    Console.WriteLine("{0} is prime!", i);
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
